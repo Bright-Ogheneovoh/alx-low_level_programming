e4c11738c4569b8ceabbfc8ba46f3b9fc9b03dbd
Bright-Ogheneovoh <godwinemeka23@gmail.com>
2022-09-14 04:43:01 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,14 +4,14 @@
  * main - prints all alphabets without q and e
  *
  * Description: prints them in lower case
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-if(ch != 'q' && ch != 'e')
+if (ch != 'q' && ch != 'e')
 {
 	putchar(ch);
 }
