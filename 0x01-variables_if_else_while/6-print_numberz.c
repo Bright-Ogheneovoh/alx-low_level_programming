beffe34c1025eac1f154191e445153b8dbeebfe6
Bright-Ogheneovoh <godwinemeka23@gmail.com>
2022-09-14 05:36:51 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers between 0 and 9
+ *
+ * Description: use the main function
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
